2e7257c6077eb99023e69e9ea88fa36752f023d4
Sam Brown <jumpbro@hotmail.com>
2022-08-17 19:44:20 +1000

    Init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react'
 function App() {
+
+  const [hero, setHero] = useState({})
+  async function random(){
+    try {
+        const response = await fetch('https://simple-overwatch-hero-api.herokuapp.com/api/random')
+        const data = await response.json()
+        setHero({
+          name: data.name, 
+          birthName: data.birthName, 
+          age: data.age, 
+          nationality: data.nationality, 
+          role: data.role, 
+          health: data.health})
+    }
+    catch(err){
+        console.log(err)
+    }
+    }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +28,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4>Name: {hero.name}</h4>
+        <h4>Birth Name: {hero.birthName}</h4>
+        <h4>Age: {hero.age}</h4>
+        <h4>Nationality: {hero.nationality}</h4>
+        <h4>Role: {hero.role}</h4>
+        <h4>Health: {hero.health}</h4>
+
+        <button onClick={random}>Click</button>
       </header>
     </div>
   );
