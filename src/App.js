54159976f16ce5e95b0be071537d42c56beea2a1
Sam Brown <jumpbro@hotmail.com>
2022-08-18 19:14:01 +1000

    allow user input to search

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react'
 function App() {
 
   const [hero, setHero] = useState({})
+  const [userInput, setUserInput] = useState('')
   async function random(){
     try {
         const response = await fetch('https://simple-overwatch-hero-api.herokuapp.com/api/random')
@@ -17,12 +18,37 @@ function App() {
           role: data.role, 
           health: data.health,
           img: data.img,
-        
         })
     }
     catch(err){
         console.log(err)
     }
+    }
+
+
+    function handleChangeUserInput(e){
+      setUserInput(e.target.value)
+    }
+
+    async function handleSearch(){
+      try {
+        const choice = userInput.toLowerCase()
+        const response = await fetch('https://simple-overwatch-hero-api.herokuapp.com/api/characters/'+choice)
+        const data = await response.json()
+
+        setHero({
+          name: data.name, 
+          birthName: data.birthName, 
+          age: data.age, 
+          nationality: data.nationality, 
+          role: data.role, 
+          health: data.health,
+          img: data.img,
+        })
+    }
+      catch(err){
+          console.log(err)
+      }
     }
 
   return (
@@ -37,9 +63,17 @@ function App() {
         <h4>Nationality: {hero.nationality}</h4>
         <h4>Role: {hero.role}</h4>
         <h4>Health: {hero.health}</h4>
-        {/* <img src=></img> */}
 
         <button onClick={random}>Find Random Hero</button>
+        <form onSubmit={e => e.preventDefault()}>
+          <input 
+            placeholder="enter hero name"
+            value={userInput}
+            onChange={handleChangeUserInput}
+          />
+          <button onClick={handleSearch}>Search</button>
+        </form>
+
       </section>
     </div>
   );
