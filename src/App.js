c3b1e9e8ef48187fb3053a4ce1f359918d44ac09
Sam Brown <jumpbro@hotmail.com>
2022-08-18 18:33:23 +1000

    css changes

--- src/App.js
+++ src/App.js
@@ -8,13 +8,17 @@ function App() {
     try {
         const response = await fetch('https://simple-overwatch-hero-api.herokuapp.com/api/random')
         const data = await response.json()
+        console.log(data)
         setHero({
           name: data.name, 
           birthName: data.birthName, 
           age: data.age, 
           nationality: data.nationality, 
           role: data.role, 
-          health: data.health})
+          health: data.health,
+          img: data.img,
+        
+        })
     }
     catch(err){
         console.log(err)
@@ -23,20 +27,20 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <section 
+        className="MainSection" 
+        style={{  backgroundImage: `url(${hero.img})`}}
+      >
         <h4>Name: {hero.name}</h4>
         <h4>Birth Name: {hero.birthName}</h4>
         <h4>Age: {hero.age}</h4>
         <h4>Nationality: {hero.nationality}</h4>
         <h4>Role: {hero.role}</h4>
         <h4>Health: {hero.health}</h4>
+        {/* <img src=></img> */}
 
-        <button onClick={random}>Click</button>
-      </header>
+        <button onClick={random}>Find Random Hero</button>
+      </section>
     </div>
   );
 }
